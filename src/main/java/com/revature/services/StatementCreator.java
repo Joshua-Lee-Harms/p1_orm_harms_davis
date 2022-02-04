c757c09f44dccd057f50d7fdecb81674f1698448
Anthony-Davis <adavi00@outlook.com>
2022-02-04 12:18:32 -0500

    updating all files local and remote

--- src/main/java/com/revature/services/StatementCreator.java
+++ src/main/java/com/revature/services/StatementCreator.java
@@ -19,9 +19,62 @@ public class StatementCreator<T> {
 	private String statement;
 
 	public StatementCreator(){}
-
-	public void buildTable(Object o) {}
-
+	
+	// UPDATE METHOD
+	
+	public String buildInitialTable(T object) throws MissingAnnotationException {
+		if (!object.getClass().isAnnotationPresent(Table.class)){
+			throw new MissingAnnotationException("@Table annotation is missing.");
+		}
+		String tableName = ReflectInfo.getTableName(object);
+		String pk = ReflectInfo.getFieldNames(object)[0];
+		
+		Field[] fields = object.getClass().getDeclaredFields();
+		List<String> foreignKey = new ArrayList<>();
+		List<String> cols = new ArrayList<>();
+		
+		
+		for (Field f : fields) {
+			String colName, type, constraint;
+			String columnString = "";
+			
+			if (f.isAnnotationPresent(Column.class)) {
+				colName = f.getName();
+				type = f.getAnnotation(Column.class).type().toString();
+				//System.out.println("pk:\t"+pk);System.out.println("col:\t"+colName);System.out.println("type:\t"+type);
+				
+				// NOT ACCESSING BELOW IF STATEMENTS
+				if (type.equals( SqlDataType.VARCHAR.toString() )) columnString = " " +colName+ " " +type+ "(20), ";
+				else if (type.equals("DOUBLE")) columnString = colName+ " " +type+ " precision, ";
+				else if (type.equals("INTEGER")) columnString = colName+ " " +type+ ", ";
+				cols.add(columnString);
+			} else {
+				throw new MissingAnnotationException(f + " missing annotation @Column");
+			}
+			
+			if (f.isAnnotationPresent(ForeignKey.class)) {
+				String fkTable = f.getAnnotation(ForeignKey.class).foreignKey();
+				// WILL NEED TO FIX VALUE BELOW IF FOREIGN KEY IS IMPLEMENTED
+				String fkColName = f.getName();
+				
+				String fkString = "foreign Key (" + colName + ") references " + fkTable + " (" + fkColName + "), ";
+				foreignKey.add(fkString);
+			}
+		}
+		String createTableSql = "create table if not exists " +tableName+ " ( ";
+		
+		for (String c : cols){
+			createTableSql += c;
+		}
+		for (String fk: foreignKey){
+			createTableSql += fk;
+		}
+		//createTableSql += "primary key(" +pk+ "))";
+		createTableSql = createTableSql.substring(0, createTableSql.length()-2);
+		createTableSql += ");";
+		System.out.println(createTableSql);
+		return  createTableSql;
+	}
 	
 	public String create(T object){
 		this.object = object;
