42a4b115c4c9cb6d4c3a1be503083fcebed3618b
Anthony-Davis <98992117+Anthony-Davis@users.noreply.github.com>
2022-02-04 00:16:28 -0500

    Add files via upload

---
+++ p1_orm_harms_davis/src/main/java/com/revature/annotations/PrimaryKey.java
@@ -0,0 +1,10 @@
+package com.revature.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public @interface PrimaryKey {
+    public String primaryKey();
+}
