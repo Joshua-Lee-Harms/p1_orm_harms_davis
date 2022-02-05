b77686acdc1176286e87f5f325a9321edec0aa7c
Anthony-Davis <adavi00@outlook.com>
2022-02-05 18:30:14 -0500

    updating all files local and remote

---
+++ src/test/java/TestModel.java
@@ -0,0 +1,76 @@
+
+
+import com.revature.annotations.Column;
+import com.revature.annotations.Table;
+import com.revature.util.SqlDataType;
+
+
+@Table(tableName = "test_model")
+public class TestModel {
+	
+	@Column(name = "id", type = SqlDataType.INTEGER)
+	private int id;
+	@Column(name = "name", type = SqlDataType.VARCHAR)
+	private String name;
+	@Column(name = "some_num", type = SqlDataType.DOUBLE)
+	private double some_num;
+	@Column(name = "other_num", type = SqlDataType.INTEGER)
+	private int other_num;
+	
+	public TestModel() {}
+	
+	public TestModel(int id, String name, double someNum, int otherNum) {
+		this.id = id;
+		this.name = name;
+		this.some_num = someNum;
+		this.other_num = otherNum;
+	}
+	public TestModel(Integer id, String name, Double someNum, Integer otherNum) {
+		this.id = id;
+		this.name = name;
+		this.some_num = someNum;
+		this.other_num = otherNum;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getSomeNum() {
+		return some_num;
+	}
+	
+	public void setSomeNum(double someNum) {
+		this.some_num = someNum;
+	}
+	
+	public int getOtherNum() {
+		return other_num;
+	}
+	
+	public void setOtherNum(int otherNum) {
+		this.other_num = otherNum;
+	}
+	
+	@Override
+	public String toString() {
+		return "Entity{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", someNum=" + some_num+//someNum +
+				", otherNum=" + other_num+//otherNum +
+				'}';
+	}
+}
---
+++ src/test/java/testing.java
@@ -0,0 +1,73 @@
+
+import com.revature.exceptions.MissingAnnotationException;
+import com.revature.exceptions.ResourceNotFoundException;
+import com.revature.repositories.Repository;
+import com.revature.services.StatementCreator;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class testing {
+	
+	public static void main(String[] args) throws ResourceNotFoundException {
+		
+		// Entity no longer inherits Repository
+		Repository<Object> repo = new Repository();
+		
+		TestModel tm = new TestModel();
+		tm.setId(1);
+		tm.setName("name1");
+		tm.setSomeNum(1.1);
+		tm.setOtherNum(11);
+		
+		try {
+			repo.initializeTable(tm);
+		} catch (SQLException | MissingAnnotationException e) {
+			e.printStackTrace();
+		}
+		
+		repo.addItem(tm);
+		
+		TestModel t2 = new TestModel();
+		t2.setId(2);
+		t2.setName("name2");
+		t2.setSomeNum(2.2);
+		t2.setOtherNum(22);
+		repo.addItem(t2);
+		
+		//System.out.println("get 2:\t"+ repo.getItem(2, tm) +"\n");
+		try {
+			//System.out.println("getAll:\t"+ repo.getAll( tm, ConnectionFactory.getConnection() ));
+			//System.out.println( repo.getAll(t2) );
+			System.out.println( repo.getAll(tm) );
+			
+		} catch (InstantiationException | IllegalAccessException | NullPointerException e) {
+			e.printStackTrace();
+		} finally {
+			//repo.deleteItem(1, tm);
+			//repo.deleteItem(2, tm);
+		}
+		
+		repo.update(tm, 1, "name", "anthony");
+		repo.update(tm, 1, "other_num", 55) ;
+		// tests for Repository class. works for adding things to postgres
+		//Repository<Entity> repo = new Repository();
+		//repo.addItem(tm);
+		//repo.deleteItem(1);
+		//System.out.println("getItem:\t" + repo.getItem(1) );
+		//System.out.println();
+		
+		// tests for StatementCreator class
+		//StatementCreator<Object> sc = new StatementCreator<>();
+		//System.out.println(sc.update(t2, 1, "name", "anthony"));
+		//System.out.println("\nStatement Creator:");
+		// Tests for initialize table method
+		//try { sc.buildInitialTable(tm); }
+		//catch (MissingAnnotationException e) { e.printStackTrace(); }
+		
+		//System.out.println( sc.create(tm) );
+		//System.out.println( sc.delete(tm) );
+		//System.out.println("\ncurrently Table name:" + sc.translate() );	//successful print of table name
+		
+	}
+}
