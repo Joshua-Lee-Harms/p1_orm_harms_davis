7f0fcdd39a066b242e6776c62f331958792105ea
Anthony-Davis <adavi00@outlook.com>
2022-02-05 20:29:10 -0500

    updating all files local and remote

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,24 +1,10 @@
 package com.revature;
 
-import com.revature.exceptions.MissingAnnotationException;
-import com.revature.repositories.Repository;
 import com.revature.util.ConnectionFactory;
-import com.revature.util.TestModel;
-
-import java.sql.SQLException;
-
 
 public class Driver {
 	
 	public static void main(String[] args) {
 		ConnectionFactory.getConnection();
-		Repository<Object> repo = new Repository<>();
-		TestModel tm = new TestModel();
-		
-		try {
-			repo.initializeTable(tm);
-		} catch (SQLException | MissingAnnotationException e) {
-			e.printStackTrace();
-		}
 	}
 }
--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -34,6 +34,7 @@ public class ConnectionFactory {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("Connection established successfully\n");
 		return connection;
 	}
 	
